75c9b8ba8a64837953292e4609ff30fc782f1609
bgquizassoc <orionsport@abv.bg>
2025-11-05 23:30:28 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quiz-cache-v1';
+const CACHE_NAME = 'quiz-cache-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -12,6 +12,16 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
@@ -21,3 +31,5 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
+
