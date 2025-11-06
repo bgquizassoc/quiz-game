eec9b39583b450eeea3e79605f8fa46d6251715b
bgquizassoc <orionsport@abv.bg>
2025-11-07 00:39:21 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quiz-cache-v11';
+const CACHE_NAME = 'quiz-cache-v12';
 const urlsToCache = [
   './',
   './index.html',
@@ -31,6 +31,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
